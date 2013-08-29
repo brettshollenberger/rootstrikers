7df3fceffd68f9ee610e424de0053f68280e7419
Aaron Frey <aaron@facultycreative.com>
2013-08-29 14:28:21 -0400

    Hooked up the Join button on the front end

--- client/app/scripts/directive/auth/registerLink.js
+++ client/app/scripts/directive/auth/registerLink.js
@@ -4,7 +4,7 @@ angular
     function() {
       return {
         replace: true,
-        template: '<a href="#" form-modal template="app/templates/auth/register.html" title="Register" ok-button-text="Register" form-submit="register()" form-clear="clear()" form-object="formUser" form-errors="errors">Register</a>'
+        template: '<a href="#" class="btn" id="join" form-modal template="app/templates/auth/register.html" title="Join" ok-button-text="Join" form-submit="register()" form-clear="clear()" form-object="formUser" form-errors="errors">Join</a>'
       };
     }
   ]);
