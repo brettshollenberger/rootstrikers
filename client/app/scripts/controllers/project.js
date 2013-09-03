21008544143cc97a8e2f64f03321f811ecb135c1
Aaron Frey <aaron@facultycreative.com>
2013-09-03 12:36:08 -0400

    Add the action kit page info to the project if it exists

--- client/app/scripts/controllers/project.js
+++ client/app/scripts/controllers/project.js
@@ -4,9 +4,22 @@ angular
     '$scope',
     '$routeParams',
     'projectService',
-    function($scope, $routeParams, projectAPI) {
+    'actionKitService',
+    function($scope, $routeParams, projectAPI, actionKitService) {
       projectAPI.getByName($routeParams.name, function(err, res) {
+        
+        // get the project from our backend
         $scope.project = res;
+        
+        // check to see if there is an actionkit page set and get that page if so
+        if($scope.project.shortname) {
+            actionKitService.getPage($scope.project.shortname).then(function(response){
+                if(response) {
+                    $scope.project.actionkit = response;
+                }
+            });
+        }
+        
       });
     }
   ]);
