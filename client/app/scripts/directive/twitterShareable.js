a211c3559d07b5081180e42ded22e7a6d58dd6d1
Brett Shollenberger <brett.shollenberger@gmail.com>
2013-09-09 15:08:56 -0400

    Modularize shareable

--- client/app/scripts/directive/twitterShareable.js
+++
@@ -1,9 +0,0 @@
-angular
-  .module('app')
-  .directive('twitterShareable', function() {
-    return {
-      restrict: 'A',
-      replace: true,
-      templateUrl: 'app/templates/components/twitterShareable.html'
-    };
-  });
