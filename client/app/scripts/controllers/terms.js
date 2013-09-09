fdfb5d309e5e2c4a4b433736c0f35b2c07a81462
Brett Shollenberger <brett.shollenberger@gmail.com>
2013-09-09 12:01:39 -0400

    Add Terms controller

---
+++ client/app/scripts/controllers/terms.js
@@ -0,0 +1,10 @@
+angular
+  .module('app')
+  .controller('termsController', [
+    '$scope',
+    '$routeParams',
+    'userService',
+    '$rootScope',
+    function($scope, $routeParams, userService, $rootScope) {
+      
+    }]);
