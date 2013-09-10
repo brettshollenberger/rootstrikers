17e8894f44c8c8efd6cb7b44b938ed6c46b91a42
Brett Shollenberger <brett.shollenberger@gmail.com>
2013-09-10 12:16:02 -0400

    Setup default image & make directive more readable

--- client/app/scripts/directive/removeImage.js
+++ client/app/scripts/directive/removeImage.js
@@ -6,14 +6,17 @@ angular
       replace: true,
       templateUrl: 'app/templates/components/removeImageButton.html',
       link: function(scope, element, attrs) {
+        // Find ViewModel object using the string passed in as the addImage value:
+        Model = scope.$eval(attrs.removeImage);
+
         element.bind('click', function() {
           // Evaluate the passed value of the removeImage attribute,
-          // and set its image and InkBlob properties to null.
-          // In this way, we can create a reusable module that
+          // and set its image and InkBlob properties to null or a default
+          // image value. In this way, we can create a reusable module that
           // allows us to pass a value, like "feature" or "project", into the
           // directive, and have it applied on the current scope. 
-          scope.$eval(attrs.removeImage).image = null;
-          scope.$eval(attrs.removeImage).InkBlob = null;
+          Model.image = Model.defaultImage;
+          Model.InkBlob = null;
           scope.$apply();
         });
       }
