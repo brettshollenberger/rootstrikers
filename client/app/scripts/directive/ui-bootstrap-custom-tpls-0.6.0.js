86ed996dff63751f904e83ebe48a3759783864a2
Brett Shollenberger <brett.shollenberger@gmail.com>
2013-09-12 14:18:09 -0400

    Style carousel

--- client/app/scripts/directive/ui-bootstrap-custom-tpls-0.6.0.js
+++ client/app/scripts/directive/ui-bootstrap-custom-tpls-0.6.0.js
@@ -950,8 +950,8 @@ angular.module("template/carousel/carousel.html", []).run(["$templateCache", fun
     "        <li ng-repeat=\"slide in slides()\" ng-class=\"{active: isActive(slide)}\" ng-click=\"select(slide)\"></li>\n" +
     "    </ol>\n" +
     "    <div class=\"carousel-inner\" ng-transclude></div>\n" +
-    "    <a ng-click=\"prev()\" class=\"carousel-control left\" ng-show=\"slides().length > 1\">&lsaquo;</a>\n" +
-    "    <a ng-click=\"next()\" class=\"carousel-control right\" ng-show=\"slides().length > 1\">&rsaquo;</a>\n" +
+    "    <a ng-click=\"prev()\" class=\"carousel-control left\" ng-show=\"slides().length > 1\"><i class=\"icon-chevron-left\"></i></a>\n" +
+    "    <a ng-click=\"next()\" class=\"carousel-control right\" ng-show=\"slides().length > 1\"><i class=\"icon-chevron-right\"></i></a>\n" +
     "</div>\n" +
     "");
 }]);
