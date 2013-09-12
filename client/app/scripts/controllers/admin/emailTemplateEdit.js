20781523c75ff9b2b3ef04a076e63bd35db9bc91
Mauricio Saglietto <msaglietto@gmail.com>
2013-09-12 11:41:52 -0300

    [#55565906] Add email preview page

--- client/app/scripts/controllers/admin/emailTemplateEdit.js
+++ client/app/scripts/controllers/admin/emailTemplateEdit.js
@@ -7,12 +7,32 @@ angular
     'emailService',
     '$location',
     function($scope, notification, $routeParams, emailAPI, $location) {
-      var model;
+      var model, tmpl = function(str, data) {
+          var fn = new Function("obj",
+            "var p=[],print=function(){p.push.apply(p,arguments);};" +
+
+            // Introduce the data as local variables using with(){}
+            "with(obj){p.push('" +
+
+            // Convert the template into pure JavaScript
+            str
+            .replace(/[\r\t\n]/g, " ")
+            .split("{{").join("\t")
+            .replace(/((^|}})[^\t]*)'/g, "$1\r")
+            .replace(/\t(.*?)}}/g, "',$1,'")
+            .split("\t").join("');")
+            .split("}}").join("p.push('")
+            .split("\r").join("\\'") + "');}return p.join('');");
+
+          return fn(data);
+        };
+
       //User is only alowed to edit a email template
       if ($routeParams.emailID) {
         emailAPI.get($routeParams.emailID, function(email) {
           model = email;
           $scope.email = model;
+          $scope.body = tmpl(model.body || '', $scope.test);
         });
       } else {
         //Not found
@@ -21,6 +41,18 @@ angular
 
       //set the model on the scope so its filled by the form
       $scope.email = model;
+      $scope.test = {
+        first_name: 'Jhon',
+        last_name: 'Doe',
+        email: 'jhon@doe.com',
+        verification_link: $location.host() + '/verify/aseqwj12312nsk123',
+        city: 'A City',
+        state: 'The State',
+        country: 'Country',
+        avatar: 'http://placehold.it/400x400',
+        thumb: 'http://placehold.it/200x200'
+
+      };
 
       //Opions for body field
       $scope.tinymceOptions = {
