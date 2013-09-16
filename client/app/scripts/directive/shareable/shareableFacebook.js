08d0a5545806438932091f71207b51e1e823a5d5
Brett Shollenberger <brett.shollenberger@gmail.com>
2013-09-16 16:02:07 -0400

    Add shareable Facebook attribute for flexible shareable directive

---
+++ client/app/scripts/directive/shareable/shareableFacebook.js
@@ -0,0 +1,16 @@
+angular
+  .module('app')
+  .directive('shareableFacebook', function() {
+    return {
+      restrict: 'A',
+      // Load the shareable attributes in a slightly higher priority
+      // than the main shareable module, so that all shareable
+      // attributes are loaded in the main shareable link function
+      priority: 1,
+      replace: true,
+      require: "^shareable",
+      link: function(scope, element, attr, shareableController) {
+        shareableController.addFacebook();
+      }
+    };
+  });
