c5594511ec41b721b13d55a7b21c3e89033e6a0c
Matt Miller <matt@facultycreative.com>
2013-10-03 15:13:34 -0400

    Get actions by project id, or all. Use native mongoose model to access .exec() functionality (ie: use properly)

--- server/lib/api/action.js
+++ server/lib/api/action.js
@@ -1,5 +1,45 @@
+var mongoose = require('mongoose')
+  , Action = mongoose.model('action')
+  ;
+
 module.exports = function(app, db) {
 
+  //***************************// Get all actions //*********************************//
+  
+  // 
+  var getActionUsers = function(req, res, next) {
+  
+    // get params and query string vars
+    var projectId = req.params.projectId || null;
+    var limit = req.query.limit || false;
+    
+    // build where clause, if we have a project id
+    var where = projectId ? { project_id: projectId } : {}; 
+    
+    //console.log('getting %s actions for project %s ', limit, projectId);
+      
+    Action
+        .find(where)
+        .sort('-date_created')
+        .limit(limit)
+        .populate({
+            path: 'user_id'
+          , select: 'email'
+        })
+        .exec(function(err, actions) {
+            if(err) return res.json(error);
+            
+            res.json(actions);
+        });
+  };
+  
+  // routes with middleware
+  app.get('/api/projects/:projectId/actions', getActionUsers);
+  app.get('/api/actions', getActionUsers);
+  
+  
+  
+  
   //***************************// Has User Acted //*********************************//
   app.get('/api/actions/hasUserActed', function(req, res) {
   
