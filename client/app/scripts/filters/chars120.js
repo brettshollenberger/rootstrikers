7193dd652dbcdb0705ce709b5c187e4dc01f6b68
Brett Shollenberger <brett.shollenberger@gmail.com>
2013-10-04 16:54:08 -0400

    Truncate preview text

--- client/app/scripts/filters/chars120.js
+++ client/app/scripts/filters/chars120.js
@@ -2,6 +2,6 @@ angular
   .module('app')
   .filter('chars120', function() {
     return function(text) {
-      return _.first(text, 120).join('') + "...";
+      return _.first(text, 120).join('') + _.chain(text.slice(120).split(' ')).first().value() + "...";
     };
   });
