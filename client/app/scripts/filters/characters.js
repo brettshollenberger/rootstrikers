efac1c9c168b242c4fae369be0ee87a039dae981
Brett Shollenberger <brett.shollenberger@gmail.com>
2013-10-04 17:09:09 -0400

    Ensure filter isn't used without text

--- client/app/scripts/filters/characters.js
+++ client/app/scripts/filters/characters.js
@@ -2,6 +2,8 @@ angular
   .module('app')
   .filter('characters', function() {
     return function(text, characters) {
-      return _.first(text, characters).join('') + _.chain(text.slice(characters).split(' ')).first().value() + "...";
+      if (text && characters) {
+        return _.first(text, characters).join('') + _.chain(text.slice(characters).split(' ')).first().value() + "...";
+      }
     };
   });
