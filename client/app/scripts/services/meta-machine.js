870d438d162750d7b3b0586702ec551352200c10
Brett Shollenberger <brett.shollenberger@gmail.com>
2013-10-04 10:49:12 -0400

    Update MetaMachine directive to add a metaImage tag and metaUrl tag (OG tags for Facebook). Directive now also sets page defaults.

--- client/app/scripts/services/meta-machine.js
+++ client/app/scripts/services/meta-machine.js
@@ -1,13 +1,31 @@
 angular
   .module('app')
   .factory('MetaMachine', function($rootScope) {
+
+    var metaDefaults = {
+      metaTitle: "Home | Rootstrikers",
+      metaDescription: "We fight the corrupting influence of money in politics",
+      metaImage: "http://facultycreative.com/img/icons/facultyicon114.png",
+      metaUrl: "http://rs002dev.herokuapp.com/"
+    };
+
+    (function setDefaults() {
+      _.each(metaDefaults, function(val, key) { $rootScope[key] = val; });
+    })();
+
     var MetaMachine = {
       title: function(pageTitle, baseTitle) {
         baseTitle = typeof baseTitle != 'undefined' ? baseTitle : "Rootstrikers";
-        $rootScope.title = typeof pageTitle != 'undefined' ? pageTitle + " | " + baseTitle : baseTitle;
+        $rootScope.metaTitle = typeof pageTitle != 'undefined' ? pageTitle + " | " + baseTitle : baseTitle;
       },
       description: function(description) {
         $rootScope.metaDescription = description || "We fight the corrupting influence of money in politics";
+      },
+      image: function(url) {
+        $rootScope.metaImage = url || "http://facultycreative.com/img/icons/facultyicon114.png";
+      },
+      url: function(url) {
+        $rootScope.metaUrl = url || "http://rs002dev.herokuapp.com/";
       }
     };
     return MetaMachine;
