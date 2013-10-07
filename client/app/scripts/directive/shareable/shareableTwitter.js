ebf3504e4d0f7b035827ac436f1a5f7ba9e55bcc
Matt Miller <matt@facultycreative.com>
2013-10-07 15:41:42 -0400

    Fix

--- client/app/scripts/directive/shareable/shareableTwitter.js
+++ client/app/scripts/directive/shareable/shareableTwitter.js
@@ -11,6 +11,10 @@ angular
       require: "^shareable",
       link: function(scope, element, attrs, shareableController) {
         shareableController.addTwitter();
+        
+        var removeHash = function(url) {
+            return url.replace('#!/', '');
+        };
 
         // If no image is specified, it falls back on the FontAwesome icon
         // in the layout
@@ -19,8 +23,15 @@ angular
         // build the url for sharing
         var twitterUrl = 'https://twitter.com/intent/tweet?hashtags=rootstrikers' + 
         '&original_referer=' +
-        encodeURIComponent(shareableController.absUrl) + 
-        '&url=' + encodeURIComponent(shareableController.absUrl);
+        encodeURIComponent((shareableController.absUrl)) + 
+        '&url=' + encodeURIComponent(removeHash(shareableController.absUrl));
+        
+     /*
+   
+        https://twitter.com/intent/tweet?hashtags=rootstrikers&original_referer=https%3A%2F%2Ftwitter.com%2Fabout%2Fresources%2Fbuttons&text=Twitter%20%2F%20Twitter%20buttons&tw_p=tweetbutton&url=http%3A%2F%2Frs.com%2Fproject&via=mattmillerart
+        
+        https://twitter.com/intent/tweet?hashtags=rootstrikers&original_referer=http%3A%2F%2Frs002dev.herokuapp.com%252F%2523%2521%252Fproject%2Fvoters-not-donors&url=http%3A%2F%2Frs002dev.herokuapp.com%252F%2523%2521%252Fproject%2Fvoters-not-donors
+*/
             
         console.log(twitterUrl);
         
