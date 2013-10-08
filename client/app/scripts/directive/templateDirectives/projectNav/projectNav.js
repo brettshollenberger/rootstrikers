690d67ae0ff028b71d2e2128d5576117450e376b
Brett Shollenberger <brett.shollenberger@gmail.com>
2013-10-07 20:01:21 -0400

    Refactor project nav to work again; separates presentational/logic concerns better

--- client/app/scripts/directive/templateDirectives/projectNav/projectNav.js
+++ client/app/scripts/directive/templateDirectives/projectNav/projectNav.js
@@ -15,35 +15,44 @@ angular
               $scope.currentProject = _.find($scope.projects, function(project) {
                 return project.slug == $routeParams.name;
               });
-
-              $scope.displayedProject = "";
-
-              t.findNextProject = function() {
-                $scope.displayedProject = $scope.projects[
-                  _.indexOf($scope.projects, $scope.displayedProject) + 1] || _.first($scope.projects);
-                $scope.$apply();
-              };
-
-              t.findPreviousProject = function() {
-                $scope.displayedProject = $scope.projects[
-                  _.indexOf($scope.projects, $scope.displayedProject) - 1] || _.last($scope.projects);
-                $scope.$apply();
-              };
-
-              t.viewAllCampaigns = function() {
-                $scope.displayedProject = {title: "View All Campaigns"};
-                $scope.$apply();
-              };
-
-              t.noAction = function() {
-                $scope.displayedProject = {title: ""};
-                $scope.$apply();
-              };
             });
           })(this);
         },
         link: function(scope, element, attrs) {
-          scope.navigateProjects = function() {
+
+          scope.displayNextProject = function() {
+            scope.displayedProject = scope.projects[
+              _.indexOf(scope.projects, scope.currentProject) + 1] || _.first(scope.projects);
+            scope.$apply();
+          };
+
+          scope.displayPreviousProject = function() {
+            scope.displayedProject = scope.projects[
+              _.indexOf(scope.projects, scope.currentProject) - 1] || _.last(scope.projects);
+            scope.$apply();
+          };
+
+          scope.displayNone = function() {
+            scope.displayedProject = {title: ""};
+            scope.$apply();
+          };
+
+          scope.displayAllCampaigns = function() {
+            scope.displayedProject = {title: "View All Campaigns"};
+            scope.$apply();
+          };
+          
+          scope.nextProject = function() {
+            navigateProject();
+            scope.displayNextProject();
+          };
+
+          scope.previousProject = function() {
+            navigateProject();
+            scope.displayPreviousProject();
+          };
+
+          navigateProject = function() {
             var slug = scope.displayedProject.slug;
             $location.path("/project/" + slug);
 
